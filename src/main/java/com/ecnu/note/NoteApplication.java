e6dcece15fa8036f980f1d167362ebfce20d2fd4
onion <969023014@qq.com>
2020-11-06 15:59:55 +0800

    delete target

--- src/main/java/com/ecnu/note/NoteApplication.java
+++ src/main/java/com/ecnu/note/NoteApplication.java
@@ -11,5 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NoteApplication {
     public static void main(String[] args) {
         SpringApplication.run(NoteApplication.class, args);
+
     }
 }
